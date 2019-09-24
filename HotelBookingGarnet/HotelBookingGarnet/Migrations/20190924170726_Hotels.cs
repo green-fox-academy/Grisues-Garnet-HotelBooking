e2e083a5ee17d5c456b018a0d5622a2f6d2df615
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-09-24 19:22:10 +0200

    add : migrations fixed

---
+++ HotelBookingGarnet/HotelBookingGarnet/Migrations/20190924170726_Hotels.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelBookingGarnet.Migrations
+{
+    public partial class Hotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    HotelId = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    HotelName = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    Region = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    StarRating = table.Column<int>(nullable: false),
+                    PropertyType = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    UserId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.HotelId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
