307dde27c6b2a0cb4435c821cf1397e20ed69c5d
Adam Kocso <adam@kocso.com>
2019-09-24 11:31:27 +0200

    add : (Hotel ViewModel)

---
+++ HotelBookingGarnet/HotelBookingGarnet/ViewModels/HotelViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingGarnet.ViewModels
+{
+    public class HotelViewModel
+    {
+        [Required]
+        public string HotelName { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Region { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public int StarRating { get; set; }
+        [Required]
+        public string PropertyType { get; set; }
+        
+    }
+}
