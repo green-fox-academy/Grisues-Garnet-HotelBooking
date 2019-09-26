46d4496863f5134b810bb7755c34ac3acc55a9c3
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-09-26 16:46:54 +0200

    modified unused items

--- HotelBookingGarnet/HotelBookingGarnet/ApplicationContext.cs
+++ HotelBookingGarnet/HotelBookingGarnet/ApplicationContext.cs
@@ -6,26 +6,10 @@ namespace HotelBookingGarnet
     public class ApplicationContext : DbContext
     {
 
-        public DbSet<Hotel> Hotels { get; set; }
-        public DbSet<PropertyType> PropertyTypes { get; set; }
-
         public ApplicationContext(DbContextOptions options) : base(options)
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<HotelPropertyType>()
-                .HasKey(a => new {a.HotelId, a.PropertyTypeId});
-            modelBuilder.Entity<HotelPropertyType>()
-                .HasOne(a => a.Hotel)
-                .WithMany(a => a.HotelPropertyTypes)
-                .HasForeignKey(a => a.HotelId);
-            modelBuilder.Entity<HotelPropertyType>()
-                .HasOne(a => a.PropertyType)
-                .WithMany(a => a.HotelPropertyTypes)
-                .HasForeignKey(a => a.PropertyTypeId);
-            base.OnModelCreating(modelBuilder);
-        }
+       
     }
 } 
