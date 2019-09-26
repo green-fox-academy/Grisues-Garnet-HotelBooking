9f38c5850fddff6bc54ea3581029bbf96ccd8f97
Péter <horv.peti90@gmail.com>
2019-09-26 14:29:59 +0200

    feat(HotelBooking): modified Usermanager

--- HotelBookingGarnet/HotelBookingGarnet/Controllers/Hotel/HotelController.cs
+++ HotelBookingGarnet/HotelBookingGarnet/Controllers/Hotel/HotelController.cs
@@ -10,18 +10,23 @@ namespace HotelBookingGarnet.Controllers.Hotel
     public class HotelController : Controller
     {
         private readonly IHotelService hotelService;
+        private readonly UserManager<User> userManager;
+        private readonly IPropertyTypeService propertyTypeService;
 
-        public HotelController(IHotelService hotelService)
+        public HotelController(IHotelService hotelService, UserManager<User> userManager, IPropertyTypeService propertyTypeService)
         {
             this.hotelService = hotelService;
+            this.userManager = userManager;
+            this.propertyTypeService = propertyTypeService;
         }
 
         [HttpPost("/info/{HotelId}")]
-        public async Task<IActionResult>HotelInfo(long HotelId)
+        public async Task<IActionResult> HotelInfo(long HotelId)
         {
-           
-           var hotel = await hotelService.FindHotelByIdAsync(HotelId);
-            return View(hotel);
+            var currentUser = await userManager.GetUserAsync(HttpContext.User);
+            var hotel = await hotelService.FindHotelByIdAsync(HotelId);asdasd
+            var property = await propertyTypeService.FindByIdAsync(hotel.PropertyTypeId);
+            return View(new IndexViewModel { user = currentUser, hotel = hotel, propertyType = property });
         }
     }
 }
