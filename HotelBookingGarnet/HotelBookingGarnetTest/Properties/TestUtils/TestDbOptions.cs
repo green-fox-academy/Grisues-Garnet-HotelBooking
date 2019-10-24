f88cdc8548d47066fa8287035bc4ca7f7ea69ea4
B707H <tothbalint90@gmail.com>
2019-10-24 17:04:30 +0200

    refactor: Test project removed

---
+++ HotelBookingGarnet/HotelBookingGarnetTest/Properties/TestUtils/TestDbOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdentiyCheckTest.TestUtils
+{
+    class TestDbOptions
+    {
+        public static DbContextOptions<ApplicationDbContext> Get()
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "blog-testdb")
+                .Options;
+        }
+    }
+}
