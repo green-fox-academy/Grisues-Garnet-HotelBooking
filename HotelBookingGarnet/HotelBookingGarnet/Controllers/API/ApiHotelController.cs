bdc412b774115d002141e9e3b01e46d5b30920b9
Adam Kocso <adam@kocso.com>
2019-11-06 11:24:06 +0100

    add : (api controllers)

---
+++ HotelBookingGarnet/HotelBookingGarnet/Controllers/API/ApiHotelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using HotelBookingGarnet.Services;
+using HotelBookingGarnet.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingGarnet.Controllers.API
+{
+    [ApiController]
+    public class ApiHotelController : ControllerBase
+    {
+        private readonly IHotelService hotelService;
+        private readonly IHotelPropertyTypeService hotelPropertyTypeService;
+
+        public ApiHotelController(IHotelService hotelService, IHotelPropertyTypeService hotelPropertyTypeService)
+        {
+            this.hotelService = hotelService;
+            this.hotelPropertyTypeService = hotelPropertyTypeService;
+        }
+
+        [HttpPut("/api/edithotel/{hotelId}")]
+        public async Task<ActionResult> EditHotel([FromBody]HotelViewModel editHotel, [FromRoute] long hotelId)
+        {
+            await hotelService.EditHotelAsync(hotelId, editHotel);
+            await hotelPropertyTypeService.EditPropertyTypeAsync(hotelId, editHotel.PropertyType);
+            var hotel = await hotelService.FindHotelByIdAsync(hotelId);
+
+            return Ok(hotel);
+        }
+    }
+}
---
+++ HotelBookingGarnet/HotelBookingGarnet/Controllers/API/ApiRoomController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using HotelBookingGarnet.Services;
+using HotelBookingGarnet.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingGarnet.Controllers.API
+{
+    [ApiController]
+    public class ApiRoomController : ControllerBase
+    {
+        private readonly IRoomService roomService;
+
+        public ApiRoomController(IRoomService roomService)
+        {
+            this.roomService = roomService;
+        }
+
+        [HttpPost("/api/addroom/{hotelId}")]
+        public async Task<ActionResult> AddRoom([FromBody] RoomViewModel newRoom, [FromRoute] long hotelId)
+        {
+            await roomService.AddRoomAsync(newRoom, hotelId);
+            return Ok();
+        }
+    }
+}
